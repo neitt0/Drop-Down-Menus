47dc8d83113ed2b4f824ae7347fcc786b97edf98
Thien Nguyen <114895619+neitt0@users.noreply.github.com>
2024-04-16 01:40:19 -0400

    add smooth transition

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const mainContainer = document.querySelector('.main-container')
 const prompt = document.querySelector('.prompt')
+const options = document.querySelectorAll('.options')
+const arrow = document.querySelector('.arrow')
 
-prompt.addEventListener('click', () => {
-  mainContainer.classList.toggle('full-size')
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    mainContainer.classList.toggle('full-size')
+    arrow.classList.toggle('opened')
+
+    // replace text with option selected
+    prompt.children[0].innerText = option.innerText
+  })
 })
