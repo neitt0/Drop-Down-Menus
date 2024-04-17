bb9510eaaa81b71644e23f94c341ff1a3992ab2b
Thien Nguyen <114895619+neitt0@users.noreply.github.com>
2024-04-16 23:35:56 -0400

    implement display image on hover

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const mainContainer = document.querySelector('.main-container')
 const prompt = document.querySelector('.prompt')
 const options = document.querySelectorAll('.options')
 const arrow = document.querySelector('.arrow')
+const fruitImgContainer = document.querySelector('.fruit-images')
+const fruitImages = fruitImgContainer.getElementsByTagName('img')
+
 
 options.forEach(option => {
   option.addEventListener('click', () => {
     mainContainer.classList.toggle('full-size')
     arrow.classList.toggle('opened')
-
+    
     // replace text with option selected
     prompt.children[0].innerText = option.innerText
   })
+
+  for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('mouseover', () => {
+      fruitImages[i].style.visibility = 'visible'  
+    })
+    options[i].addEventListener('mouseout', () => {
+      fruitImages[i].style.visibility = 'hidden'  
+    })
+  }
 })
