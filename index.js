72a9751e1be42f87558062d830e8c5220aacd9ad
Thien Nguyen <114895619+neitt0@users.noreply.github.com>
2024-04-23 04:26:45 -0400

    Revert "added transitions"
    This reverts commit d9308ee9f6a4322a4fd2bb4567883230665d3aa1.

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ options.forEach(option => {
 
   for (let i = 0; i < options.length; i++) {
     options[i].addEventListener('mouseover', () => {
-      fruitImages[i].style.transition = '.05s ease-in'
-
-      fruitImages[i].style.opacity = '100'  
+      fruitImages[i].style.visibility = 'visible'  
     })
     options[i].addEventListener('mouseout', () => {
-      fruitImages[i].style.transition = '.3s ease-out'
-      fruitImages[i].style.opacity = '0'  
+      fruitImages[i].style.visibility = 'hidden'  
     })
   }
 })
